5cd82ef6ca74b13fb9ac0ee68b2fec6666ce1694
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-28 00:29:29 +0000

    Fix: PEP8

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -17,8 +17,8 @@ class square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())

1d3e7b1b465fa9f3a5ced92cc756a809c22be684
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-28 00:48:37 +0000

    Fix: PEP8

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,12 +2,13 @@
 """
 Square class
 """
+
+
 class square():
     """Square class"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -17,13 +18,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of the square """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
